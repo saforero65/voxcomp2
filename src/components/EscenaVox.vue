58fd910c0e1ed72e81aaca038f11add602c2bdc4
gabrielaasl <gabrielasofialeon@gmail.com>
2022-05-18 11:43:06 -0500

    Menus

--- src/components/EscenaVox.vue
+++ src/components/EscenaVox.vue
@@ -66,7 +66,7 @@ export default {
       this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
       this.scene = new THREE.Scene();
-      this.scene.background = new THREE.Color(0xf0f0f0);
+      this.scene.background = new THREE.Color(0x000000);
 
       // roll-over helpers
 
@@ -94,8 +94,8 @@ export default {
       this.gridHelper = new THREE.GridHelper(
         size,
         divisions,
-        0x39ff14,
-        0x39ff14
+        0xffffff,
+        0xffffff
       );
       this.scene.add(this.gridHelper);
 
@@ -108,7 +108,7 @@ export default {
 
       this.plane = new THREE.Mesh(
         geometry,
-        new THREE.MeshBasicMaterial({ visible: false })
+        new THREE.MeshBasicMaterial({ visible: false, color: 0xf0f0f0})
       );
       this.scene.add(this.plane);
       this.objects.push(this.plane);
